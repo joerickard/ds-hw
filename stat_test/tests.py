0f4898d9b2a56b44968c3a93ba6a24c01255c62f
Jordan Boyd-Graber <jordanbg@gmail.com>
2016-10-04 20:14:17 -0600

    adding test

--- stat_test/tests.py
+++ stat_test/tests.py
@@ -48,7 +48,7 @@ class TestBigramFinder(unittest.TestCase):
         bigrams = set(self._bf_loaded.valid_bigrams())
         self.assertEqual(bigrams, set([('b', 'c'), ('a', 'b')]))
             
-    def testPValue(self):
+    def testChiSquarePValue(self):
         obs = array([[36, 14], [30, 25]])
         ex = array([[31.43, 18.57], [34.57, 20.43]])
 
@@ -56,14 +56,20 @@ class TestBigramFinder(unittest.TestCase):
             
     def testVar(self):
         x = [9, 2, 1]
-        self.assertAlmostEqual(unbiased_sample_variance(x, 4), 9.5)
+        self.assertAlmostEqual(unbiased_sample_variance(x, 4), 19.0)
 
     def testDegreesOfFreedom(self):
-        self.assertAlmostEqual(degrees_of_freedom(1, 2, 4, 8), 42/5.)
+        self.assertAlmostEqual(degrees_of_freedom(1, 2, 4, 8), 9 * 42/38.)
 
     def testStatistic(self):
         self.assertAlmostEqual(t_statistic(27.15, 11.95, 20, 20, 156.5, 213.7444),
                                3.532761644279526, places=3)
+
+    def testTPValue(self):
+        v1 = [5, 7, 5, 3, 5, 3, 3, 9]
+        v2 = [8, 1, 4, 6, 6, 4, 1, 2]
+
+        self.assertAlmostEqual(t_test(v1, v2), 0.26822842114830348, places=3)
                     
 if __name__ == '__main__':
     unittest.main()
