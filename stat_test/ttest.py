61784968f6a3e5fde7d7c76193fc97e57d56031f
joe rickard <joe.s.rickard@gmai.com>
2016-10-13 14:10:45 -0600

    ttests

--- stat_test/ttest.py
+++ stat_test/ttest.py
@@ -1,5 +1,4 @@
 from math import sqrt
-
 from scipy.stats import t
 from numpy import mean
 
@@ -12,27 +11,29 @@ def degrees_of_freedom(s1, s2, n1, n2):
     @param n1 Thu number of observations in the first sample
     @param n2 The number of observations in the second sample
     """
-
-
-    return 0.0
+    Num = ((s1**2/n1)+(s2**2/n2))**2
+    Den = (((s1**2/n1)**2/(n1-1)) + ((s2**2/n2)**2/(n2-1)))
+    return Num/Den
 
 def unbiased_sample_variance(observations, mean):
     """
     Compute the unbiased sample variance
-
     @param observations Iterable set of observations
     @param mean The estimated mean
     """
-    
-    return 0
+
+    summation = 0
+    for x in observations:
+        summation += (x-mean)**2
+    return (1/(len(observations)-1)*summation)
 
 def t_statistic(mean1, mean2, n1, n2, svar1, svar2):
     """
     Compute the t-statistic for the given estimates
     """
-
-    # Complete this funtion
-    return 0.0
+    M = mean1-mean2
+    SE = math.sqrt((svar1**2)/n1 + (svar2**2)/n2)
+    return M/SE
 
 def t_test(sample1, sample2):
     """
@@ -41,10 +42,9 @@ def t_test(sample1, sample2):
     @param sample1 An iterable of the first sample
     @param sample2 An iterable of the second sample
     """
-
-
-
-    return 0.0
+    tStat = t_statistic(np.mean(sample1), np.mean(sample2), len(sample1), len(sample2), unbiased_sample_variance(sample1, mean1), unbiased_sample_variance(sample2, mean2))
+    DofF = degrees_of_freedom(unbiased_sample_variance(sample1, mean1), unbiased_sample_variance(sample2, mean2), len(sample1), len(sample2))
+    return t.sf(tStat, DofF)*2
 
 if __name__ == "__main__":
     v1 = [5, 7, 5, 3, 5, 3, 3, 9]
